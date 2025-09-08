cb324c8d752e65fd9d62c6217638627f9e611494
chrisETML <christopher.ristic@yahoo.fr>
2025-09-08 14:30:00 +0200

    Initial commit

---
+++ MeteoStats/Form1.cs
@@ -0,0 +1,10 @@
+namespace MeteoStats
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MeteoStats/MainMeteoStats.cs
@@ -0,0 +1,23 @@
+///ETML
+///Auteur : Christopher Ristic 
+///Date: 08.09.2025
+///Description: Programme principale de l'application MeteoStats
+
+
+namespace MeteoStats
+{
+    internal static class MainMeteoStats
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
